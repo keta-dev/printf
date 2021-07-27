29df39d4381a1dd1a34507e1e6b1f231c339da87
keta-dev <preyedan5@yahoo.com>
2021-07-27 21:41:48 +0100

    modified prototype and putchar

--- holberton.h
+++ holberton.h
@@ -1,43 +1,32 @@
-#ifndef HOLBERTON_H
-#define HOLBERTON_H
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H_
+
 #include <stdarg.h>
-
 /**
- * struct format_specifiers - struct holing each format specifier with
- * its callback
- * @specifier: the format specifier
- * @printer: callback for printing the argument
+ * struct print - structure for printing various types
+ * @t: type to print
+ * @f: function to print
  */
-typedef struct format_specifiers
+typedef struct print
 {
-	char *specifier;
-	int (*printer)(va_list arg_list);
-} fs;
+	char *t;
+	int (*f)(va_list);
+} print_t;
 
-int print_R(va_list arg);
-int print_rev(va_list arg);
-int print_STR(va_list arg);
-int print_memory(va_list arg_list);
 int _putchar(char c);
-int parser(const char *format, va_list arg_list);
 int _printf(const char *format, ...);
-int print_char(va_list);
-int print_string(va_list);
-int print_decimal(va_list);
-int print_hex(va_list);
-int print_HEX(va_list);
-int _pow(unsigned int base, int power);
-int print_octal(va_list arg_list);
-char *inttostr(int number);
-int print_unsignedTobinary(int num);
-int print_binary(va_list arg_list);
-int _strlen(char *string);
-int intlen(int number);
-int isoctal(char *string);
-int octaltodecimal(char *string);
-int fs_looper(const char *format, const char **formatt, va_list arg_list,
-	      fs format_list[]);
-int format_looper(const char *format, va_list arg_list, fs format_list[]);
-int print_unsigned(va_list arg_list);
+int print_c(va_list c);
+int print_s(va_list s);
+int print_i(va_list i);
+int print_d(va_list d);
+int print_u(va_list u);
+int print_b(va_list b);
+int print_o(va_list o);
+int print_x(va_list x);
+int print_X(va_list X);
+int print_p(va_list p);
+int print_S(va_list S);
+int print_r(va_list r);
+int print_R(va_list R);
 
-#endif
+#endif  /* _HOLBERTON_H */
